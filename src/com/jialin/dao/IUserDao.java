a3c8a2f8ac5a7b66672f08beb0222b1d988ef8f2
SYSU-Jmiao <jmiao@mail2.sysu.edu.cn>
2014-07-02 16:02:42 +0800

    初始配置

---
+++ src/com/jialin/dao/IUserDao.java
@@ -0,0 +1,10 @@
+package com.jialin.dao;
+
+import com.jialin.entity.User;
+
+public interface IUserDao {
+	public void AddUser(User user);
+	public void modifyUser(User user);
+	public void deleteUser(User user);
+	public User findUserByName(String name); 
+}
