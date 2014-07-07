5bd0483c9fcc090e53ba73ad656adeb2c2af195e
SYSU-Jmiao <jmiao@mail2.sysu.edu.cn>
2014-07-07 15:05:03 +0800

    delete

--- src/com/jialin/service/IUserManage.java
+++
@@ -1,9 +0,0 @@
-package com.jialin.service;
-
-import com.jialin.entity.User;
-
-public interface IUserManage {
-	public void addUser(User user);
-	public void modifyUser(User user);
-	public void deleteUser(User user);
-}
