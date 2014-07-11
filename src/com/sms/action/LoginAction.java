b5eae074d1b71eba0a1d78bb01f2dc70917cbd59
SYSU-Jmiao <jmiao@mail2.sysu.edu.cn>
2014-07-11 09:52:02 +0800

    debug

--- src/com/sms/action/LoginAction.java
+++ src/com/sms/action/LoginAction.java
@@ -12,6 +12,7 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 import com.sms.dao.impl.UserDaoImp;
 import com.sms.entity.User;
+import com.sms.security.Md5;
 import com.sms.service.IUserManage;
 import com.sms.service.impl.UserManageImp;
 
@@ -64,20 +65,6 @@ public class LoginAction extends ActionSupport {
 
 		HttpServletRequest request = ServletActionContext.getRequest();
 		System.out.println(request.getCharacterEncoding());
-
-<<<<<<< HEAD
-=======
-		if ("root".equals(user.getId())
-				&& "root123".equals(user.getPassword())) {
-			Map session = ActionContext.getContext().getSession();
-			session.put("user.name", user.getId());
-
-			System.out.println("��½�ɹ����û���=" + user.getId());
-			return "success";
-		}
-
-		System.out.println("��½ʧ�ܣ��û���=" + user.getId());
->>>>>>> 356acf907cbbb57ff27cc08173e35017323c8ac8
 		String userIdString = user.getId().toString(), UserPassword = user.getPassword();
 		
 		if (!isInteger(userIdString))
