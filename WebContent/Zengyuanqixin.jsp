1d26890400f66adc005a1258f49e7d700599f610
Wongxing <945502104@qq.com>
2014-07-17 11:58:40 +0800

    NOTHING

--- WebContent/Zengyuanqixin.jsp
+++ WebContent/Zengyuanqixin.jsp
@@ -51,17 +51,17 @@
 	
 	起薪时间(XXXX-XX-XX):<input type="text" name="startSalaryInfo.startSalaryDate" /><br />
 	基本工资是否上调10%：<input type="radio" name="startSalaryInfo.isRaised" value="true" checked="checked"/>是<input type="radio" name="startSalaryInfo.isRaised" value="false"/>否<br />
-	套改年限：<input type="text" name="startSalaryInfo.changeYears" /><br />
+	套改年限（自动套用）：<input type="text" name="startSalaryInfo.changeYears" /><br />
 	中断工龄：<input type="text" name="startSalaryInfo.breakOffSeniority" /><br />
 	截至2006年6月30日实际工作年限：<input type="text" name="startSalaryInfo.workYears" /><br />
-	工作前工龄：<input type="text" name="startSalaryInfo.seniorityBeforeWork" /><br />
+	工作前工龄（自动套用）：<input type="text" name="startSalaryInfo.seniorityBeforeWork" /><br />
 	大专以上不计工龄年限：<input type="text" name="startSalaryInfo.learnSeniority" /><br />
-	任职年限：<input type="text" name="startSalaryInfo.hireYears" /><br />
+	任职年限（自动套用）：<input type="text" name="startSalaryInfo.hireYears" /><br />
 	工资系列：<input type="text" name="startSalaryInfo.salarySeries" /><br />
 	工资岗位级别：<input type="text" name="startSalaryInfo.positionLevel" /><br />
-	薪级：<input type="text" name="startSalaryInfo.salaryLevel" /><br />
-	岗位工资：<input type="text" name="startSalaryInfo.positionSalary" /><br />
-	薪级工资：<input type="text" name="startSalaryInfo.levelSalary" /><br />
+	薪级（自动套用）：<input type="text" name="startSalaryInfo.salaryLevel" /><br />
+	岗位工资（自动套用）：<input type="text" name="startSalaryInfo.positionSalary" /><br />
+	薪级工资（自动套用）：<input type="text" name="startSalaryInfo.levelSalary" /><br />
 	备注：<input type="text" name="startSalaryInfo.remarks" value="无" onfocus="if (value =='无'){value =''}" onblur="if (value ==''){value='无'}"/><br />
 	
 	<input type="button" name="btnadd" onclick="importStaff()" value="增加" />
--- src/com/sms/action/ZengyuanqixinAction.java
+++ src/com/sms/action/ZengyuanqixinAction.java
@@ -8,6 +8,7 @@ import com.opensymphony.xwork2.ActionSupport;
 import com.sms.entity.Employee;
 import com.sms.entity.StartSalaryInfo;
 import com.sms.service.IEmployeeManage;
+import com.sms.service.IManageSalaryManage;
 import com.sms.service.IStartSalaryInfoManage;
 import com.sms.action.EmployeeAction;
 
@@ -54,6 +55,17 @@ public class ZengyuanqixinAction extends ActionSupport {
 	public IEmployeeManage getEmployeeManage() {
 		return employeeManage;
 	}
+	
+	@Resource
+	private IManageSalaryManage manageSalaryManage;	
+	
+	public IManageSalaryManage getManageSalaryManage() {
+		return manageSalaryManage;
+	}
+
+	public void setManageSalaryManage(IManageSalaryManage manageSalaryManage) {
+		this.manageSalaryManage = manageSalaryManage;
+	}
 
 	public static boolean isValid(int value) {
 		if (value >= 100000 && value <= 999999)
@@ -69,6 +81,40 @@ public class ZengyuanqixinAction extends ActionSupport {
 			employeeManage.addEmployee(employee);
 			startSalaryInfo.setEid(employee.getId());
 			startSalaryInfo.setOperateDate(new Date());
+			
+			//自动套用确定工作前年龄
+			if (employee.getMaxDegree() == "博士")
+				startSalaryInfo.setSeniorityBeforeWork(5);
+			else if (employee.getMaxDegree() == "硕士")
+				startSalaryInfo.setSeniorityBeforeWork(2);			
+			startSalaryInfo.setChangeYears(startSalaryInfo.getWorkYears()+startSalaryInfo.getSeniorityBeforeWork()+startSalaryInfo.getLearnSeniority());
+			
+			startSalaryInfo.setHireYears(2006-employee.getStartWorkDate().getYear()+1-startSalaryInfo.getBreakOffSeniority());
+			
+			if (startSalaryInfo.getSalarySeries() == "管理")
+			{
+				startSalaryInfo.setPositionSalary(new Double(manageSalaryManage.findManPosSalByLevel(startSalaryInfo.getPositionLevel()).getSalaryStandard()));
+				Integer 
+			}
+			else if (startSalaryInfo.getSalarySeries() == "专技")
+			{
+				
+			}
+			else if (startSalaryInfo.getSalarySeries() == "技工")
+			{
+				
+			}
+			else if (startSalaryInfo.getSalarySeries() == "普工")
+			{
+				
+			}
+			else 
+			{
+				System.out.println("工资系列有误，请您检查");
+				return "fail";
+			}
+			
+			
 			startSalaryInfoManage.addStartSalaryInfo(startSalaryInfo);
 			return "success";
 		}
